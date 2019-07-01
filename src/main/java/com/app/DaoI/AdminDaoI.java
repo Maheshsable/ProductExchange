32eaea80be60351808ab53ffbdfb160478b987ae
Mahesh <maheshsable33@gmail.com>
2019-07-01 22:27:30 +0530

    second commit

--- src/main/java/com/app/DaoI/AdminDaoI.java
+++ src/main/java/com/app/DaoI/AdminDaoI.java
@@ -1,5 +1,14 @@
 package com.app.DaoI;
 
+import java.util.ArrayList;
+
+import com.app.Entity.Admin;
+import com.app.Entity.Country;
+
 public interface AdminDaoI {
+
+	Integer saveAdmin(Admin admin);
+
+	ArrayList<Country> getCountry();
 
 }
--- src/main/java/com/app/DaoI/UserDaoI.java
+++ src/main/java/com/app/DaoI/UserDaoI.java
@@ -1,5 +1,11 @@
 package com.app.DaoI;
 
+import com.app.Entity.User;
+
 public interface UserDaoI {
+
+	Integer loginUser(String name, String password);
+
+	Integer saveUser(User user);
 
 }
